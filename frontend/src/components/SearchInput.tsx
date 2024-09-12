7137d3e57df1d7861eccf925fc265517b158e9b2
Mohsin370 <mohsinijaz13@gmail.com>
2024-09-12 13:25:42 +0930

    auto complete with map api for address added

--- frontend/src/components/SearchInput.tsx
+++ frontend/src/components/SearchInput.tsx
@@ -1,40 +1,88 @@
 import React from "react";
-import {Input} from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem, Input } from "@nextui-org/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { autoCompleteAddress } from "../utils/api";
+import { useAsyncList } from "@react-stately/data";
 
 export default function SearchInput() {
+  type IAddress = {
+    geometry: {
+      coordinates: Array<number>;
+    };
+    properties: {
+      full_address: string;
+      mapbox_id: string;
+      feature_type: string;
+      name: string;
+      place_formatted: string;
+    };
+  };
+
+  let list = useAsyncList<IAddress>({
+    async load({ filterText }) {
+      if (
+        filterText === undefined ||
+        filterText === "" ||
+        filterText.length < 2
+      ) {
+        return {
+          items: [],
+        };
+      }
+      let addresses = await autoCompleteAddress(filterText);
+      return {
+        items: addresses.features || [],
+      };
+    },
+  });
+
+  const processLocationname = (item: IAddress) => {
+    let res: string = "";
+    let { properties } = item;
+    switch (properties.feature_type) {
+      case "address": {
+        res = properties.full_address;
+        break;
+      }
+      case "poi": {
+        res = properties.name;
+        break;
+      }
+      case "place":
+      case "street": {
+        res = properties.name + ", " + properties.place_formatted;
+        break;
+      }
+    }
+
+    return <div>{res}</div>;
+  };
+
   return (
     <div className="w-[360px] m-auto my-3 rounded-xl flex justify-center items-center  shadow-lg  text-white">
-      <Input
-        label="Search"
-        isClearable
-        radius="lg"
+      <Autocomplete
+        label="Find restaurants"
+        placeholder="Search an address"
+        inputValue={list.filterText}
+        onInputChange={list.setFilterText}
+        items={list.items}
+        isLoading={list.isLoading}
         classNames={{
-          label: "text-black/50 dark:text-white/90",
-          input: [
-            "bg-transparent",
-            "text-black/90 dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focus=true]:bg-default-200/50",
-            "dark:group-data-[focus=true]:bg-default/60",
-            "!cursor-text",
-          ],
+          base: "text-black/50 dark:text-white/90",
         }}
-        placeholder="Type to search..."
-        startContent={
-            <MagnifyingGlassIcon  className="h-4 w-4 text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+        selectorIcon={
+          <MagnifyingGlassIcon className="h-4 w-4 text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
         }
-      />
+        listboxProps={{
+          emptyContent: "No Address Found",
+        }}
+      >
+        {(item) => (
+          <AutocompleteItem key={item.properties.mapbox_id}>
+            {processLocationname(item)}
+          </AutocompleteItem>
+        )}
+      </Autocomplete>
     </div>
   );
 }
---
+++ frontend/src/utils/api/mapBoxSearch.ts
@@ -0,0 +1,27 @@
+const MAPBOX_URL = process.env.NEXT_PUBLIC_MAPBOX_URL+"/search";
+const VERSION = "v1";
+const ACCESS_TOKEN= process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+export const autoCompleteAddress = async (search_text: string) => {
+  
+  try{
+
+    if(!ACCESS_TOKEN){
+      throw new Error("ACCESS TOKEN is missing");
+    }
+
+    const params = new URLSearchParams({
+      q: search_text,
+      limit: '4',
+      access_token: ACCESS_TOKEN
+    });
+
+    const response = await fetch(`${MAPBOX_URL}/searchbox/${VERSION}/forward?${params}`, { cache: "no-store" });
+    if (!response.ok) throw new Error("Failed to fetch locations");
+    return response.json();
+  
+  }catch(error){
+    console.log(error);
+    return [];
+  }
+};
