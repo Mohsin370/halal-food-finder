fd1089b1176fa2eff1a44b1108666d61170d580e
Mohsin370 <mohsinijaz13@gmail.com>
2025-02-28 17:28:05 +1030

    backend test deployment

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -17,8 +17,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
