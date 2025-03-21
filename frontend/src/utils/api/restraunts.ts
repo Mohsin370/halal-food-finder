11e9d3d398bc827f5308e978385e289dcb0a2339
Mohsin370 <mohsinijaz13@gmail.com>
2025-03-21 12:00:03 +1030

    test

--- frontend/src/utils/api/restraunts.ts
+++ frontend/src/utils/api/restraunts.ts
@@ -7,7 +7,6 @@ const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 export const fetchRcentRestaurants = async () => {
   try {
     const response = await fetch(`${BASE_URL}/Restaurants/recent`);
-    if (!response.ok) throw new Error("Failed to fetch recent restaurants");
     return response.json();
   } catch (error) {
     console.log(error);
@@ -18,7 +17,6 @@ export const fetchRcentRestaurants = async () => {
 export const fetchFeaturedRestaurants = async () => {
   try {
     const response = await fetch(`${BASE_URL}/Restaurants/featured`);
-    if (!response.ok) throw new Error("Failed to fetch featured restaurants");
     return response.json();
   } catch (error) {
     console.log(error);
