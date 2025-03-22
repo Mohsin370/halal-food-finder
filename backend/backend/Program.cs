c5e9eb033fd6c0946901280df536acf88600e8e9
Mohsin370 <mohsinijaz13@gmail.com>
2025-03-23 03:08:26 +1030

    run migrations

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -32,6 +32,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ServerContext>();
+    dbContext.Database.Migrate();  
+}
+
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
