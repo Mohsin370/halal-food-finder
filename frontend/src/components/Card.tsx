898a1a48585867978a7bda546e94a9c0ac747b1f
Mohsin370 <mohsinijaz13@gmail.com>
2025-03-23 21:29:26 +1030

    use truncate for longer text

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -3,16 +3,16 @@ import { Image } from "@heroui/image";
 
 const ScrollCard: React.FC<{ restaurant: RestaurantT }> = ({ restaurant }) => {
   return (
-    <div className="mr-3 my-3 w-[300px]">
-      <Card className="py-4" isPressable={true}>
+    <div className="mr-3 my-3 w-[350px]">
+      <Card className="py-4 flex" isPressable={true}>
         <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <h4 className="font-semibold text-large/6 break-words w-full whitespace-normal text-left">{restaurant.name}</h4>
+          <h4 className="font-semibold truncate w-[320px]">{restaurant.name} das d sad a das dsa d asd asd a d asd as</h4>
           <p className="text-tiny uppercase font-semibold">{restaurant.restaurantType.name}</p>
           <small className="text-default-500">
             {restaurant.suburb}, {restaurant.city}
           </small>
         </CardHeader>
-        <CardBody className="overflow-visible py-2 ">
+        <CardBody className="overflow-visible py-2 text-end ">
           <Image alt="Card background" className="object-cover rounded-xl" src={restaurant.image} width={500} height={200} />
         </CardBody>
       </Card>
