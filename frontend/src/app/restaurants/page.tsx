82e0dab157b7377160bed8aa0c528d97874ea5f8
Mohsin370 <mohsinijaz13@gmail.com>
2025-03-24 14:00:21 +1030

    type error fix

--- frontend/src/app/restaurants/page.tsx
+++ frontend/src/app/restaurants/page.tsx
@@ -1,11 +1,9 @@
 import { fetchRestaurantsListing, getRestaurantlookUps } from "../../utils/api";
 import Listing from "./Listing";
 
-interface RestaurantProps {
-  searchParams: { cuisineType?: string };
-}
 
-const Restaurant = async ({ searchParams }: RestaurantProps) => {
+
+const Restaurant = async ({ searchParams }: any) => {
   const params = await searchParams;
   const restaurants = await fetchRestaurantsListing(parseInt(params.cuisineType?params.cuisineType:"0"));
   const lookups = await getRestaurantlookUps();
