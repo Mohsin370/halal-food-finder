bcc60cede422cc49b1b5cf4a9fc048557493aa08
Mohsin370 <mohsinijaz13@gmail.com>
2025-04-01 15:33:21 +1030

    pause db migration

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -37,7 +37,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ServerContext>();
-    dbContext.Database.Migrate();  
+    //dbContext.Database.Migrate();  
 }
 
 
